1b96530cc7f81d2343297a088447811986f4140b
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2024-06-04 12:45:53 +1000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,12 +18,11 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "Classpad More+",
-    description: "welcome to Classpad More+",
+    title: "morethan-log",
+    description: "welcome to morethan-log!",
   },
-
   // CONFIG configration (required)
-  link: "https://claspad-more.vercel.app",
+  link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash

2ff7b1fe2f1d88f92d589516ce02b86ee050bbbf
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2024-06-04 12:08:52 +1000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Jason Li",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Classpad developer",
+    bio: "I develop everything for classpad",
+    email: "mulichfe@gmail.com",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [

0d61065658816bcee9eb61bbd12be41afc3ba5ac
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2024-06-04 12:37:40 +1000

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -8,6 +8,22 @@ const ServiceCard: React.FC = () => {
   if (!CONFIG.projects) return null
   return (
     <>
+      <StyledTitle>
+        <Emoji>🌟</Emoji> Service
+      </StyledTitle>
+      <StyledWrapper>
+        {CONFIG.projects.map((project, idx) => (
+          <a
+            key={idx}
+            href={`${project.href}`}
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiFillCodeSandboxCircle className="icon" />
+            <div className="name">{project.name}</div>
+          </a>
+        ))}
+      </StyledWrapper>
     </>
   )
 }
